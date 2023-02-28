ccbe7dae35324c2edfbc4e06eed2d7e9d4e82a04
Aldi Oktaviana Hidayat <aldiokta10@hotmail.co.id>
2023-02-28 09:18:48 +0700

    feat: add break points(mq)

--- constants/themes.js
+++ constants/themes.js
@@ -15,4 +15,10 @@ const themes = {
 	border_radius: "0.75rem",
 };
 
+const breakpoints = [640, 768, 1024, 1280, 1536];
+
+const mq = breakpoints.map((bp) => `@media (min-width: ${bp}px)`);
+
 export default themes;
+
+export { themes, mq };
